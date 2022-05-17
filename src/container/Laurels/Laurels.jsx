871c0f4ac8ef4bb7dcb2ae286dc80e4642feac83
Jon Pena <iamjonntech@gmail.com>
2022-05-16 21:09:31 -0700

    updated the laurel,& gallery sections

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
+import { SubHeading } from '../../components'
+import { images, data } from '../../constants'
 import './Laurels.css';
 
+const AwardCard = ({ award: {imgUrl, title, subtitle} }) => (
+  <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt='award'/>
+    <div className='app__laurels_award-card-content'>
+      <p className='p__cormorant' style={{color: '#Dcca87'}}>{title}</p>
+      <p className='p__cormorant'>{subtitle}</p>
+
+    </div>
+  </div>
+)
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className='app_wrapper_info'>
+      <SubHeading title='Awards and Recognition'/>
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+      
+      <div className='app__laurels_awards'>
+        {data.awards.map((award) => <AwardCard award={award} key={award.title}/>)}
+      </div>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.laurels} alt="laurels" />
+    </div>
   </div>
 );
 
